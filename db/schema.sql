b533fd48f4a558a9da3a53b95013698a49c694df
Savannah Mercedes <savannahmunn1996@gmail.com>
2023-04-02 20:24:21 -0700

    finished schema

--- db/schema.sql
+++ db/schema.sql
@@ -13,6 +13,7 @@ CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title Varchar(30),
   salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
   FOREIGN KEY (department_id)
   REFERENCES department(id)
   ON DELETE SET NULL
@@ -23,14 +24,10 @@ CREATE TABLE employee (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
  first_name VARCHAR(30),
   last_name VARCHAR(30),
+  role_id INT NOT NULL,
   FOREIGN KEY (role_id)
-  REFERENCES role(id)
-
-
-
-
-
-
-
-
+  REFERENCES role(id),
+  manager_id INT,
+  FOREIGN KEY (employee_id)
+  REFERENCES employee(id)
 )
